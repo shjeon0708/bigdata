2b128e899bf22cf480f05417137beb33c0f8cdec
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:10:34 +0900

    Update 5.sql

--- dbgen/queries/5.sql
+++ dbgen/queries/5.sql
@@ -1,31 +1,24 @@
--- $ID$
--- TPC-H/TPC-R Local Supplier Volume Query (Q5)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	n_name,
-	sum(l_extendedprice * (1 - l_discount)) as revenue
-from
-	customer,
-	orders,
-	lineitem,
-	supplier,
-	nation,
-	region
-where
-	c_custkey = o_custkey
-	and l_orderkey = o_orderkey
-	and l_suppkey = s_suppkey
-	and c_nationkey = s_nationkey
-	and s_nationkey = n_nationkey
-	and n_regionkey = r_regionkey
-	and r_name = ':1'
-	and o_orderdate >= date ':2'
-	and o_orderdate < date ':2' + interval '1' year
-group by
-	n_name
-order by
-	revenue desc;
-:n -1
+SELECT
+N_NAME,
+SUM(L_EXTENDEDPRICE * (1 - L_DISCOUNT)) AS REVENUE
+FROM
+	CUSTOMER,
+	ORDERS,
+	LINEITEM,
+	SUPPLIER,
+	NATION,
+	REGION
+WHERE
+C_CUSTKEY = O_CUSTKEY
+AND L_ORDERKEY = O_ORDERKEY
+AND L_SUPPKEY = S_SUPPKEY
+AND C_NATIONKEY = S_NATIONKEY
+AND S_NATIONKEY = N_NATIONKEY
+AND N_REGIONKEY = R_REGIONKEY
+AND R_NAME = 'ASIA'
+AND O_ORDERDATE >= DATE '1994-01-01'
+AND O_ORDERDATE < DATE '1994-01-01' + INTERVAL '1' YEAR
+GROUP BY
+N_NAME
+ORDER BY
+REVENUE DESC;
