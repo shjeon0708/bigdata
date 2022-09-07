0d0a148ba4f26c1389897fa9bad45f9cf4a62a85
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:13:43 +0900

    Update 16.sql

--- dbgen/queries/16.sql
+++ dbgen/queries/16.sql
@@ -1,37 +1,7 @@
--- $ID$
--- TPC-H/TPC-R Parts/Supplier Relationship Query (Q16)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	p_brand,
-	p_type,
-	p_size,
-	count(distinct ps_suppkey) as supplier_cnt
-from
-	partsupp,
-	part
-where
-	p_partkey = ps_partkey
-	and p_brand <> ':1'
-	and p_type not like ':2%'
-	and p_size in (:3, :4, :5, :6, :7, :8, :9, :10)
-	and ps_suppkey not in (
-		select
-			s_suppkey
-		from
-			supplier
-		where
-			s_comment like '%Customer%Complaints%'
-	)
-group by
-	p_brand,
-	p_type,
-	p_size
-order by
-	supplier_cnt desc,
-	p_brand,
-	p_type,
-	p_size;
-:n -1
+SELECT
+P_BRAND, P_TYPE, P_SIZE, COUNT(DISTINCT PS_SUPPKEY) AS SUPPLIER_CNT FROM PARTSUPP, PART
+WHERE P_PARTKEY = PS_PARTKEY AND P_BRAND <> 'BRAND#45'
+AND P_TYPE NOT LIKE 'MEDIUM POLISHED%'
+AND P_SIZE IN (49, 14, 23, 45, 19, 3, 36, 9) AND PS_SUPPKEY NOT IN (SELECT S_SUPPKEY FROM SUPPLIER WHERE S_COMMENT LIKE '%CUSTOMER%COMPLAINTS%') 
+GROUP BY P_BRAND, P_TYPE, P_SIZE
+ORDER BY SUPPLIER_CNT DESC, P_BRAND, P_TYPE, P_SIZE;
