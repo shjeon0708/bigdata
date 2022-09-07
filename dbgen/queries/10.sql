178da84f45aeb38cc4f370427caab7bfd6b80d4f
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:11:59 +0900

    Update 10.sql

--- dbgen/queries/10.sql
+++ dbgen/queries/10.sql
@@ -1,38 +1,31 @@
--- $ID$
--- TPC-H/TPC-R Returned Item Reporting Query (Q10)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	c_custkey,
-	c_name,
-	sum(l_extendedprice * (1 - l_discount)) as revenue,
-	c_acctbal,
-	n_name,
-	c_address,
-	c_phone,
-	c_comment
-from
-	customer,
-	orders,
-	lineitem,
-	nation
-where
-	c_custkey = o_custkey
-	and l_orderkey = o_orderkey
-	and o_orderdate >= date ':1'
-	and o_orderdate < date ':1' + interval '3' month
-	and l_returnflag = 'R'
-	and c_nationkey = n_nationkey
-group by
-	c_custkey,
-	c_name,
-	c_acctbal,
-	c_phone,
-	n_name,
-	c_address,
-	c_comment
-order by
-	revenue desc;
-:n 20
+SELECT C_CUSTKEY,
+	C_NAME,
+	SUM(L_EXTENDEDPRICE * (1 - L_DISCOUNT)) AS REVENUE,
+	C_ACCTBAL,
+	N_NAME,
+	C_ADDRESS,
+	C_PHONE,
+	C_COMMENT
+FROM
+	CUSTOMER,
+	ORDERS,
+	LINEITEM,
+	NATION
+WHERE
+	C_CUSTKEY = O_CUSTKEY
+	AND L_ORDERKEY = O_ORDERKEY
+	AND O_ORDERDATE >= DATE '1993-10-01'
+	AND O_ORDERDATE < DATE '1993-10-01' + INTERVAL '3' MONTH
+	AND L_RETURNFLAG = 'R'
+	AND C_NATIONKEY = N_NATIONKEY
+GROUP BY
+	C_CUSTKEY,
+	C_NAME,
+	C_ACCTBAL,
+	C_PHONE,
+	N_NAME,
+	C_ADDRESS,
+	C_COMMENT
+ORDER BY
+	REVENUE DESC
+LIMIT 20;
