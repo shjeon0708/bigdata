9566af133225006ff08910dceaa957ab51247a6b
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:13:57 +0900

    Update 17.sql

--- dbgen/queries/17.sql
+++ dbgen/queries/17.sql
@@ -1,24 +1,5 @@
--- $ID$
--- TPC-H/TPC-R Small-Quantity-Order Revenue Query (Q17)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	sum(l_extendedprice) / 7.0 as avg_yearly
-from
-	lineitem,
-	part
-where
-	p_partkey = l_partkey
-	and p_brand = ':1'
-	and p_container = ':2'
-	and l_quantity < (
-		select
-			0.2 * avg(l_quantity)
-		from
-			lineitem
-		where
-			l_partkey = p_partkey
-	);
-:n -1
+SELECT
+SUM(L_EXTENDEDPRICE) / 7.0 AS AVG_YEARLY
+FROM LINEITEM, PART
+WHERE P_PARTKEY = L_PARTKEY AND P_BRAND = 'BRAND#23'
+AND P_CONTAINER = 'MED BOX' AND L_QUANTITY < (SELECT 0.2 * AVG(L_QUANTITY) FROM LINEITEM WHERE L_PARTKEY = P_PARTKEY);
