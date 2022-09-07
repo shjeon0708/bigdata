33cc6a8c20edacb9de1de9e3e49f608905fd916f
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:14:25 +0900

    Update 19.sql

--- dbgen/queries/19.sql
+++ dbgen/queries/19.sql
@@ -1,42 +1,15 @@
--- $ID$
--- TPC-H/TPC-R Discounted Revenue Query (Q19)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	sum(l_extendedprice* (1 - l_discount)) as revenue
-from
-	lineitem,
-	part
-where
-	(
-		p_partkey = l_partkey
-		and p_brand = ':1'
-		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
-		and l_quantity >= :4 and l_quantity <= :4 + 10
-		and p_size between 1 and 5
-		and l_shipmode in ('AIR', 'AIR REG')
-		and l_shipinstruct = 'DELIVER IN PERSON'
-	)
-	or
-	(
-		p_partkey = l_partkey
-		and p_brand = ':2'
-		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
-		and l_quantity >= :5 and l_quantity <= :5 + 10
-		and p_size between 1 and 10
-		and l_shipmode in ('AIR', 'AIR REG')
-		and l_shipinstruct = 'DELIVER IN PERSON'
-	)
-	or
-	(
-		p_partkey = l_partkey
-		and p_brand = ':3'
-		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
-		and l_quantity >= :6 and l_quantity <= :6 + 10
-		and p_size between 1 and 15
-		and l_shipmode in ('AIR', 'AIR REG')
-		and l_shipinstruct = 'DELIVER IN PERSON'
-	);
-:n -1
+SELECT
+SUM(L_EXTENDEDPRICE* (1 - L_DISCOUNT)) AS REVENUE
+FROM LINEITEM, PART
+WHERE
+(P_PARTKEY = L_PARTKEY
+AND P_BRAND = 'BRAND#12'
+AND P_CONTAINER IN ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
+AND L_QUANTITY >= 1 AND L_QUANTITY <= 1 + 10
+AND P_SIZE BETWEEN 1 AND 5 AND L_SHIPMODE IN ('AIR', 'AIR REG')
+AND L_SHIPINSTRUCT = 'DELIVER IN PERSON')
+OR
+(P_PARTKEY = L_PARTKEY AND P_BRAND = 'BRAND#23' AND P_CONTAINER IN ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
+AND L_QUANTITY >= 10 AND L_QUANTITY <= 10 + 10 AND P_SIZE BETWEEN 1 AND 10 AND L_SHIPMODE IN ('AIR', 'AIR REG') AND L_SHIPINSTRUCT = 'DELIVER IN PERSON' )
+OR
+(P_PARTKEY = L_PARTKEY AND P_BRAND = 'BRAND#34' AND P_CONTAINER IN ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG') AND L_QUANTITY >= 20 AND L_QUANTITY <= 20 + 10 AND P_SIZE BETWEEN 1 AND 15 AND L_SHIPMODE IN ('AIR', 'AIR REG') AND L_SHIPINSTRUCT = 'DELIVER IN PERSON');
