dead525244e24b4bcb2d1a83ca174e48c25903ee
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:11:43 +0900

    Update 9.sql

--- dbgen/queries/9.sql
+++ dbgen/queries/9.sql
@@ -1,39 +1,15 @@
--- $ID$
--- TPC-H/TPC-R Product Type Profit Measure Query (Q9)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	nation,
-	o_year,
-	sum(amount) as sum_profit
-from
-	(
-		select
-			n_name as nation,
-			extract(year from o_orderdate) as o_year,
-			l_extendedprice * (1 - l_discount) - ps_supplycost * l_quantity as amount
-		from
-			part,
-			supplier,
-			lineitem,
-			partsupp,
-			orders,
-			nation
-		where
-			s_suppkey = l_suppkey
-			and ps_suppkey = l_suppkey
-			and ps_partkey = l_partkey
-			and p_partkey = l_partkey
-			and o_orderkey = l_orderkey
-			and s_nationkey = n_nationkey
-			and p_name like '%:1%'
-	) as profit
-group by
-	nation,
-	o_year
-order by
-	nation,
-	o_year desc;
-:n -1
+SELECT
+NATION, O_YEAR, SUM(AMOUNT) AS SUM_PROFIT
+FROM
+(SELECT
+N_NAME AS NATION, EXTRACT(YEAR FROM O_ORDERDATE) AS O_YEAR, L_EXTENDEDPRICE * (1 - L_DISCOUNT) - PS_SUPPLYCOST * L_QUANTITY AS AMOUNT
+FROM
+PART,SUPPLIER, LINEITEM,PARTSUPP,ORDERS,NATION
+WHERE
+S_SUPPKEY = L_SUPPKEY AND PS_SUPPKEY = L_SUPPKEY AND PS_PARTKEY = L_PARTKEY AND P_PARTKEY = L_PARTKEY AND O_ORDERKEY = L_ORDERKEY AND S_NATIONKEY = N_NATIONKEY
+AND P_NAME LIKE '%GREEN%'
+) AS PROFIT
+GROUP BY
+NATION, O_YEAR
+ORDER BY
+NATION, O_YEAR DESC;
