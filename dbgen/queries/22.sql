5c845758750076a3eeda796620ef4db13f6183d6
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:16:16 +0900

    Update 22.sql

--- dbgen/queries/22.sql
+++ dbgen/queries/22.sql
@@ -1,44 +1,7 @@
--- $ID$
--- TPC-H/TPC-R Global Sales Opportunity Query (Q22)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	cntrycode,
-	count(*) as numcust,
-	sum(c_acctbal) as totacctbal
-from
-	(
-		select
-			substring(c_phone from 1 for 2) as cntrycode,
-			c_acctbal
-		from
-			customer
-		where
-			substring(c_phone from 1 for 2) in
-				(':1', ':2', ':3', ':4', ':5', ':6', ':7')
-			and c_acctbal > (
-				select
-					avg(c_acctbal)
-				from
-					customer
-				where
-					c_acctbal > 0.00
-					and substring(c_phone from 1 for 2) in
-						(':1', ':2', ':3', ':4', ':5', ':6', ':7')
-			)
-			and not exists (
-				select
-					*
-				from
-					orders
-				where
-					o_custkey = c_custkey
-			)
-	) as custsale
-group by
-	cntrycode
-order by
-	cntrycode;
-:n -1
+SELECT
+CNTRYCODE, COUNT(*) AS NUMCUST, SUM(C_ACCTBAL) AS TOTACCTBAL
+FROM
+(SELECT SUBSTRING(C_PHONE FROM 1 FOR 2) AS CNTRYCODE, C_ACCTBAL FROM CUSTOMER WHERE
+SUBSTRING(C_PHONE FROM 1 FOR 2) IN ('13', '31', '23', '29', '30', '18', '17') AND C_ACCTBAL > ( SELECT AVG(C_ACCTBAL) FROM CUSTOMER WHERE C_ACCTBAL > 0.00 AND SUBSTRING(C_PHONE FROM 1 FOR 2) IN ('13', '31', '23', '29', '30', '18', '17')) AND NOT EXISTS ( SELECT * FROM ORDERS WHERE O_CUSTKEY = C_CUSTKEY)) AS CUSTSALE
+GROUP BY CNTRYCODE
+ORDER BY CNTRYCODE;
