317a8ef567798b24418ffeb2c506bf0160e2e413
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:15:51 +0900

    Update 20.sql

--- dbgen/queries/20.sql
+++ dbgen/queries/20.sql
@@ -1,44 +1,10 @@
--- $ID$
--- TPC-H/TPC-R Potential Part Promotion Query (Q20)
--- Function Query Definition
--- Approved February 1998
-:x
-:o
-select
-	s_name,
-	s_address
-from
-	supplier,
-	nation
-where
-	s_suppkey in (
-		select
-			ps_suppkey
-		from
-			partsupp
-		where
-			ps_partkey in (
-				select
-					p_partkey
-				from
-					part
-				where
-					p_name like ':1%'
-			)
-			and ps_availqty > (
-				select
-					0.5 * sum(l_quantity)
-				from
-					lineitem
-				where
-					l_partkey = ps_partkey
-					and l_suppkey = ps_suppkey
-					and l_shipdate >= date ':2'
-					and l_shipdate < date ':2' + interval '1' year
-			)
-	)
-	and s_nationkey = n_nationkey
-	and n_name = ':3'
-order by
-	s_name;
-:n -1
+SELECT
+S_NAME, S_ADDRESS
+FROM
+SUPPLIER, NATION
+WHERE
+S_SUPPKEY IN ( SELECT PS_SUPPKEY FROM PARTSUPP WHERE PS_PARTKEY IN (SELECT P_PARTKEY FROM PART
+WHERE P_NAME LIKE 'FOREST%') AND PS_AVAILQTY > (SELECT 0.5 * SUM(L_QUANTITY) FROM LINEITEM
+WHERE L_PARTKEY = PS_PARTKEY AND L_SUPPKEY = PS_SUPPKEY
+AND L_SHIPDATE >= DATE '1994-01-01'
+AND L_SHIPDATE < DATE '1994-01-01' + INTERVAL '1' YEAR)) AND S_NATIONKEY = N_NATIONKEY AND N_NAME = 'CANADA' ORDER BY S_NAME;
