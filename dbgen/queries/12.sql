9621bdbb578b89540c806ad44db4e010dfa0cc9f
shjeon0708 <94080990+shjeon0708@users.noreply.github.com>
2022-09-07 13:12:27 +0900

    Update 12.sql

--- dbgen/queries/12.sql
+++ dbgen/queries/12.sql
@@ -1,35 +1,9 @@
--- $ID$
--- TPC-H/TPC-R Shipping Modes and Order Priority Query (Q12)
--- Functional Query Definition
--- Approved February 1998
-:x
-:o
-select
-	l_shipmode,
-	sum(case
-		when o_orderpriority = '1-URGENT'
-			or o_orderpriority = '2-HIGH'
-			then 1
-		else 0
-	end) as high_line_count,
-	sum(case
-		when o_orderpriority <> '1-URGENT'
-			and o_orderpriority <> '2-HIGH'
-			then 1
-		else 0
-	end) as low_line_count
-from
-	orders,
-	lineitem
-where
-	o_orderkey = l_orderkey
-	and l_shipmode in (':1', ':2')
-	and l_commitdate < l_receiptdate
-	and l_shipdate < l_commitdate
-	and l_receiptdate >= date ':3'
-	and l_receiptdate < date ':3' + interval '1' year
-group by
-	l_shipmode
-order by
-	l_shipmode;
-:n -1
+SELECT
+L_SHIPMODE, SUM(CASE
+WHEN O_ORDERPRIORITY = '1-URGENT' OR O_ORDERPRIORITY = '2-HIGH' THEN 1 ELSE 0 END) AS HIGH_LINE_COUNT, SUM(CASE WHEN O_ORDERPRIORITY <> '1-URGENT' AND O_ORDERPRIORITY <> '2-HIGH' THEN 1 ELSE 0 END) AS LOW_LINE_COUNT FROM ORDERS, LINEITEM
+WHERE
+O_ORDERKEY = L_ORDERKEY AND L_SHIPMODE IN ('MAIL', 'SHIP') AND L_COMMITDATE < L_RECEIPTDATE AND L_SHIPDATE < L_COMMITDATE AND L_RECEIPTDATE >= DATE '1994-01-01' AND L_RECEIPTDATE < DATE '1994-01-01' + INTERVAL '1' YEAR
+GROUP BY
+L_SHIPMODE
+ORDER BY
+L_SHIPMODE;
